eae86c0399b62a6c342286c1b087142bdc4adfca
Christian <christiantermo84@gmail.com>
2024-06-12 23:03:58 +0200

    Modifica alla register: unificazione login e registrazione

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Log;
 class RegisterController extends Controller
 {
 
-    public function register(RegisterRequest $request, User $user)
+    public function registertest(RegisterRequest $request, User $user)
     {
         $user = User::where('telefono', $request['telefono'])->first();
 
