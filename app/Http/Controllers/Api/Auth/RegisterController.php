9db375467d46de0d5440148993a0cb4790f571ff
Christian <christiantermo84@gmail.com>
2024-06-12 20:49:49 +0200

    Modifica alla register: unificazione login e registrazione

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -17,62 +17,85 @@ use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use PhpParser\Node\Expr\Cast\Object_;
+use Illuminate\Support\Facades\Log;
 
 class RegisterController extends Controller
 {
     public function action(RegisterRequest $request)
     {
-        $user = User::where('telefono', $request['telefono'])->first();
         $telefono = $request['telefono'];
-    
-        if ($user) {
-            // Se l'utente esiste già, invia direttamente l'OTP
-            $otp = VerificationCode::create([
-                'telefono' => $request['telefono'],
-                'otp' => rand(10000, 99999),
-                'expire_at' => Carbon::now()->addMinutes(10)
+        $user = User::where('telefono', $telefono)->first();
+
+        try {
+            if ($user) {
+                // Se l'utente esiste già, invia direttamente l'OTP
+                $otp = $this->generateOtp($telefono);
+
+                // Genera il token per l'utente esistente
+                $token = Auth::login($user);
+            } else {
+                // Crea un nuovo utente
+                $user = User::create([
+                    'telefono' => $telefono,
+                    'role' => 'Regular User',
+                ]);
+                $user->assignRole('Regular User');
+
+                // Genera OTP per il nuovo utente
+                $otp = $this->generateOtp($telefono);
+
+                // Genera il token per il nuovo utente
+                $token = Auth::login($user);
+            }
+
+            // Invia il messaggio SMS
+            $message = $this->sendSms($telefono, $otp->otp);
+
+            // Restituisce la risposta JSON con l'utente e il token
+            return response()->json([
+                'user' => $user,
+                'token' => $token,
+                'message' => $message
             ]);
-    
-            // Genera il token per l'utente esistente
-            $token = Auth::login($user);
-        } else {
-            // Crea un nuovo utente
-            $user = User::create([
-                'telefono' => $telefono,
-                'role' => 'Regular User',
-            ]);
-            $user->assignRole('Regular User');
-    
-            // Genera OTP per il nuovo utente
-            $otp = VerificationCode::create([
-                'telefono' => $request['telefono'],
-                'otp' => rand(10000, 99999),
-                'expire_at' => Carbon::now()->addMinutes(10)
-            ]);
-    
-            // Genera il token per il nuovo utente
-            $token = Auth::login($user);
+        } catch (\Exception $e) {
+            // Log dell'errore
+            Log::error('Errore durante la registrazione o l\'invio dell\'OTP: ' . $e->getMessage());
+
+            // Restituisce una risposta di errore JSON
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Si è verificato un errore durante la registrazione o l\'invio dell\'OTP.'
+            ], 500);
         }
-    
-        // Invia il messaggio SMS
-        $message = Http::get('https://www.services.europsms.com/smpp-gateway.php', [
-            'op' => 'sendSMS2',
-            'smpp_id' => 'christiantermo40@gmail.com',
-            'utenti_password' => 'termo',
-            'tipologie_sms_id' => '6',
-            'destinatari_destination_addr' => $telefono,
-            'trasmissioni_messaggio' => 'Il tuo codice di verifica è: ' . $otp->otp,
-            'trasmissioni_mittente' => ''
-        ]);
-    
-        // Restituisce la risposta JSON con l'utente e il token
-        return response()->json([
-            'user' => $user,
-            'token' => $token,
-            'message' => $message->body()
+    }
+
+    private function generateOtp($telefono)
+    {
+        return VerificationCode::create([
+            'telefono' => $telefono,
+            'otp' => rand(10000, 99999),
+            'expire_at' => Carbon::now()->addMinutes(10)
         ]);
     }
 
+    private function sendSms($telefono, $otp)
+    {
+        $response = Http::get('https://www.services.europsms.com/smpp-gateway.php', [
+            'op' => 'sendSMS2',
+            'smpp_id' => env('SMPP_ID'),
+            'utenti_password' => env('SMPP_PASSWORD'),
+            'tipologie_sms_id' => '6',
+            'destinatari_destination_addr' => $telefono,
+            'trasmissioni_messaggio' => 'Il tuo codice di verifica è: ' . $otp,
+            'trasmissioni_mittente' => ''
+        ]);
+
+        if ($response->successful()) {
+            return $response->body();
+        } else {
+            throw new \Exception('Errore durante l\'invio del messaggio SMS.');
+        }
+    }
 
     public function registerProfessional(RegisterRequest $request, User $user)
     {
